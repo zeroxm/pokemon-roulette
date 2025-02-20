a9633f021b50ac6d1f8671228ee728073224848b
André Xavier Martinez <andre.martinez@pandora.com.br>
2025-02-20 01:39:08 -0300

    Better button layout and coffee screen base done

--- src/app/coffee/coffee.component.ts
+++ src/app/coffee/coffee.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import { NgIconsModule } from '@ng-icons/core';
+import { DarkModeToggleComponent } from "../dark-mode-toggle/dark-mode-toggle.component";
+import { MainGameButtonComponent } from "../main-game-button/main-game-button.component";
 
 @Component({
   selector: 'app-coffee',
-  imports: [],
+  imports: [
+    NgIconsModule,
+    DarkModeToggleComponent,
+    MainGameButtonComponent
+],
   templateUrl: './coffee.component.html',
   styleUrl: './coffee.component.css'
 })
