6ca26effc92747860f2e0f47ad161f40fce79fc1
André Xavier Martinez <andre.martinez@pandora.com.br>
2025-03-22 19:11:13 -0300

    Removendo código de teste

--- src/app/services/trainer-service/trainer.service.ts
+++ src/app/services/trainer-service/trainer.service.ts
@@ -64,12 +64,12 @@ export class TrainerService {
     //     "front_shiny": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/shiny/5.png"
     //    },
     //   shiny: false, power: 2, weight: 1 },
-    { text: "Charizard", pokemonId: 6, fillStyle: "darkred",
-      sprite: { 
-        "front_default": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/6.png",
-        "front_shiny": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/shiny/6.png"
-       },
-      shiny: false, power: 3, weight: 1 }
+    // { text: "Charizard", pokemonId: 6, fillStyle: "darkred",
+    //   sprite: { 
+    //     "front_default": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/6.png",
+    //     "front_shiny": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/shiny/6.png"
+    //    },
+    //   shiny: false, power: 3, weight: 1 }
   ];
 
   storedPokemon: PokemonItem[] = [
