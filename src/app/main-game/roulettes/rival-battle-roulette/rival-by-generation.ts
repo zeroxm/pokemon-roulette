e67305e08393d1b3bafd96fb2c0958e51669d6be
Baptiste GISLOT <baptiste.gislot@mylegitech.com>
2025-06-13 14:36:43 +0200

    feat: translated all rivals to english

--- src/app/main-game/roulettes/rival-battle-roulette/rival-by-generation.ts
+++ src/app/main-game/roulettes/rival-battle-roulette/rival-by-generation.ts
@@ -1,66 +1,67 @@
 import { GymLeader } from "../../../interfaces/gym-leader";
 
 export const rivalByGeneration: Record<number, GymLeader> = {
-    1: {
-        name: 'Blue',
-        sprite: "https://raw.githubusercontent.com/zeroxm/pokemon-roulette-trainer-sprites/refs/heads/main/sprites/Spr_B2W2_Blue.png",
-        quotes: [
-            "Let's check out our Pokémon! Come on, I'll take you on!"
-        ]
-    },
-    2: {
-        name: 'Silver',
-        sprite: "https://raw.githubusercontent.com/zeroxm/pokemon-roulette-trainer-sprites/refs/heads/main/sprites/Spr_HGSS_Silver.png",
-        quotes: [
-            "I too have a good Pokémon. I'll show you what I mean!"
-        ]
-    },
-    3: {
-        name: 'Wally',
-        sprite: "https://raw.githubusercontent.com/zeroxm/pokemon-roulette-trainer-sprites/refs/heads/main/sprites/Spr_RS_Wally.png",
-        quotes: [
-            "Please see how much my training has accomplished!"
-        ]
-    },
-    4: {
-        name: 'Barry',
-        sprite: "https://raw.githubusercontent.com/zeroxm/pokemon-roulette-trainer-sprites/refs/heads/main/sprites/Spr_Pt_Barry.png",
-        quotes: [
-            "You aren't the only one who's getting tougher! I'll prove it to you! Let's go!"
-        ]
-    },
-    5: {
-        name: 'N',
-        sprite: [
-            'https://raw.githubusercontent.com/zeroxm/pokemon-roulette-trainer-sprites/refs/heads/main/sprites/Spr_B2W2_N.png'
-        ],
-        quotes: [
-            "Let me hear your Pokémon's voice!"
-        ]
-    },
-    6: {
-        name: 'Calem/Serena',
-        sprite: [
-            "https://raw.githubusercontent.com/zeroxm/pokemon-roulette-trainer-sprites/refs/heads/main/sprites/VSCalem_2.png",
-            "https://raw.githubusercontent.com/zeroxm/pokemon-roulette-trainer-sprites/refs/heads/main/sprites/VSSerena_2.png"
-        ],
-        quotes: [
-            "I'm a Pokémon Trainer, I want to see things that make Pokémon special.",
-            "My Pokémon and I will show you what makes us special!"
-        ]
-    },
-    7: {
-        name: 'Gladion',
-        sprite: "https://raw.githubusercontent.com/zeroxm/pokemon-roulette-trainer-sprites/refs/heads/main/sprites/VSGladion.png",
-        quotes: [
-            "Battle me. I won't take no for an answer.",
-        ]
-    },
-    8: {
-        name: 'Bede',
-        sprite: "https://raw.githubusercontent.com/zeroxm/pokemon-roulette-trainer-sprites/refs/heads/main/sprites/VSBede.png",
-        quotes: [
-            "It's time to prove I'm a notch above the competition to everyone in the Galar region"
-        ]
-    },
+  1: {
+    name: 'rival.blue.name',
+    sprite: "https://raw.githubusercontent.com/zeroxm/pokemon-roulette-trainer-sprites/refs/heads/main/sprites/Spr_B2W2_Blue.png",
+    quotes: [
+      "rival.blue.quote1",
+    ]
+  },
+  2: {
+    name: 'rival.silver.name',
+    sprite: "https://raw.githubusercontent.com/zeroxm/pokemon-roulette-trainer-sprites/refs/heads/main/sprites/Spr_HGSS_Silver.png",
+    quotes: [
+      "rival.silver.quote1"
+    ]
+  },
+  3: {
+    name: 'rival.wally.name',
+    sprite: "https://raw.githubusercontent.com/zeroxm/pokemon-roulette-trainer-sprites/refs/heads/main/sprites/Spr_RS_Wally.png",
+    quotes: [
+      "rival.wally.quote1"
+    ]
+  },
+  4: {
+    name: 'rival.barry.name',
+    sprite: "https://raw.githubusercontent.com/zeroxm/pokemon-roulette-trainer-sprites/refs/heads/main/sprites/Spr_Pt_Barry.png",
+    quotes: [
+      "rival.barry.quote1"
+    ]
+  },
+  5: {
+    name: 'rival.n.name',
+    sprite: [
+      'https://raw.githubusercontent.com/zeroxm/pokemon-roulette-trainer-sprites/refs/heads/main/sprites/Spr_B2W2_N.png'
+    ],
+    quotes: [
+      "rival.n.quote1"
+    ]
+  },
+  6: {
+    name: 'rival.calem-serena.name',
+    sprite: [
+      "https://raw.githubusercontent.com/zeroxm/pokemon-roulette-trainer-sprites/refs/heads/main/sprites/VSCalem_2.png",
+      "https://raw.githubusercontent.com/zeroxm/pokemon-roulette-trainer-sprites/refs/heads/main/sprites/VSSerena_2.png"
+    ],
+    quotes: [
+      "rival.calem-serena.quote1",
+      "rival.calem-serena.quote2"
+    ]
+  },
+  7: {
+    name: 'rival.gladion.name',
+    sprite: "https://raw.githubusercontent.com/zeroxm/pokemon-roulette-trainer-sprites/refs/heads/main/sprites/VSGladion.png",
+    quotes: [
+      "rival.gladion.quote1"
+    ]
+  },
+  8: {
+    name: 'rival.bede.name',
+    sprite: "https://raw.githubusercontent.com/zeroxm/pokemon-roulette-trainer-sprites/refs/heads/main/sprites/VSBede.png",
+    quotes: [
+      "rival.bede.quote1"
+    ]
+  },
+
 }
