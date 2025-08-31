b7bd6ee86a0aed690994446c0b75f470631a9f24
André Xavier Martinez <andre.martinez@pandora.com.br>
2025-08-31 13:50:10 -0300

    Fixing previous errors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-<<<<<<< feature/translations
-import { TranslateModule, TranslateService } from '@ngx-translate/core';
-
-@Component({
-  selector: 'app-root',
-=======
 import { TranslateService, TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
   standalone: true,
->>>>>>> main
   imports: [RouterOutlet, TranslateModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
@@ -20,18 +13,14 @@ export class AppComponent {
   title = 'pokemon-roulette';
 
   constructor(private translate: TranslateService) {
-<<<<<<< feature/translations
     const savedLanguage = localStorage.getItem('language') || 'en';
     this.translate.addLangs(['en', 'fr', 'es']);
     this.translate.setDefaultLang('en');
     this.translate.use(savedLanguage);
-=======
-    translate.setDefaultLang('en');
-    translate.use('en');
   }
 
   changeLang(lang: string) {
     this.translate.use(lang);
->>>>>>> main
+    localStorage.setItem('language', lang);
   }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,19 +2,10 @@ import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } fr
 import { provideRouter } from '@angular/router';
 import { provideIcons } from '@ng-icons/core';
 import { routes } from './app.routes';
-<<<<<<< feature/translations
 import { HttpClient, provideHttpClient } from '@angular/common/http';
-import { bootstrapArrowRepeat, bootstrapCheck, bootstrapClock, bootstrapController,
-         bootstrapCupHotFill, bootstrapMap, bootstrapPcDisplayHorizontal, bootstrapPeopleFill, bootstrapShare } from '@ng-icons/bootstrap-icons';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
-import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
-
-const httpLoaderFactory: (http: HttpClient) => TranslateHttpLoader = (http: HttpClient) => new TranslateHttpLoader(http, '/i18n/', '.json');
-=======
-import { provideHttpClient, HttpClient } from '@angular/common/http';
-
 import {
   bootstrapArrowRepeat,
+  bootstrapCheck,
   bootstrapClock,
   bootstrapController,
   bootstrapCupHotFill,
@@ -23,22 +14,20 @@ import {
   bootstrapPeopleFill,
   bootstrapShare
 } from '@ng-icons/bootstrap-icons';
-
-import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
 
-// Fonction pour charger les fichiers JSON de traduction
+const httpLoaderFactory: (http: HttpClient) => TranslateHttpLoader = (http: HttpClient) => new TranslateHttpLoader(http, '/i18n/', '.json');
+
 export function HttpLoaderFactory(http: HttpClient) {
   return new TranslateHttpLoader(http, './assets/i18n/', '.json');
 }
 
 
->>>>>>> main
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-<<<<<<< feature/translations
     provideIcons(
       { bootstrapArrowRepeat,
         bootstrapCheck,
@@ -60,32 +49,5 @@ export const appConfig: ApplicationConfig = {
       },
       defaultLanguage: 'en'
     })])
-  ]
-};
-=======
-    provideIcons({
-      bootstrapArrowRepeat,
-      bootstrapClock,
-      bootstrapController,
-      bootstrapCupHotFill,
-      bootstrapPcDisplayHorizontal,
-      bootstrapPeopleFill,
-      bootstrapShare,
-      bootstrapMap
-    }),
-    provideHttpClient(),
-    provideZoneChangeDetection({ eventCoalescing: true }),
->>>>>>> main
-
-    importProvidersFrom(
-      TranslateModule.forRoot({
-        defaultLanguage: 'en',
-        loader: {
-          provide: TranslateLoader,
-          useFactory: HttpLoaderFactory,
-          deps: [HttpClient]
-        }
-      })
-    )
   ]
 };
