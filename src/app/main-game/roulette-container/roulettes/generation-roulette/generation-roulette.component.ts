ae629cef5f4b799c672b65af084ccd6338c65f30
André Xavier Martinez <andre.martinez@pandora.com.br>
2025-09-02 13:37:32 -0300

    Now it's possible to select the generation you want

--- src/app/main-game/roulette-container/roulettes/generation-roulette/generation-roulette.component.ts
+++ src/app/main-game/roulette-container/roulettes/generation-roulette/generation-roulette.component.ts
@@ -1,9 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Output } from '@angular/core';
-import {TranslatePipe} from '@ngx-translate/core';
+import { TranslatePipe } from '@ngx-translate/core';
 import { WheelComponent } from '../../../../wheel/wheel.component';
 import { GenerationService } from '../../../../services/generation-service/generation.service';
 import { GenerationItem } from '../../../../interfaces/generation-item';
+import { DarkModeService } from '../../../../services/dark-mode-service/dark-mode.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-generation-roulette',
@@ -17,15 +19,29 @@ import { GenerationItem } from '../../../../interfaces/generation-item';
 })
 export class GenerationRouletteComponent {
 
-  constructor(private generationService: GenerationService) {
+  constructor(private generationService: GenerationService,
+              private darkModeService: DarkModeService) {
     this.generations = this.generationService.getGenerationList();
+    this.darkMode = this.darkModeService.darkMode$;
   }
 
   generations: GenerationItem[];
+  darkMode!: Observable<boolean>;
   selectedGeneration: GenerationItem | null = null;
+  showChoiceButtons = false;
   @Output() generationSelectedEvent = new EventEmitter<GenerationItem>();
 
   onItemSelected(index: number): void {
+    this.selectedGeneration = this.generations[index];
+    this.generationService.setGeneration(index);
+    this.generationSelectedEvent.emit();
+  }
+
+  toggleChoiceView(): void {
+    this.showChoiceButtons = !this.showChoiceButtons;
+  }
+
+  onGenerationChosen(index: number): void {
     this.selectedGeneration = this.generations[index];
     this.generationService.setGeneration(index);
     this.generationSelectedEvent.emit();
