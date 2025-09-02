79325b8f73aecb06b6beaa8d7c9ee0f5b256d19a
André Xavier Martinez <andre.martinez@pandora.com.br>
2025-09-02 14:58:41 -0300

    Minor enhancements

--- src/app/credits/credits.component.ts
+++ src/app/credits/credits.component.ts
@@ -2,14 +2,16 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { MainGameButtonComponent } from "../main-game-button/main-game-button.component";
 import {TranslatePipe} from '@ngx-translate/core';
+import { CoffeeButtonComponent } from "../main-game/coffee-button/coffee-button.component";
 
 @Component({
   selector: 'app-credits',
   imports: [
     CommonModule,
     MainGameButtonComponent,
-    TranslatePipe
-  ],
+    TranslatePipe,
+    CoffeeButtonComponent
+],
   templateUrl: './credits.component.html',
   styleUrl: './credits.component.css'
 })
