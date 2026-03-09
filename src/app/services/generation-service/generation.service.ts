4f1cd691ea234c268754fba464f0672df2ae638f
André Xavier Martinez <andre.martinez@pandora.com.br>
2026-03-09 00:07:18 -0300

    New generation added as a option

--- src/app/services/generation-service/generation.service.ts
+++ src/app/services/generation-service/generation.service.ts
@@ -11,14 +11,15 @@ export class GenerationService {
   }
 
   private generations: GenerationItem[] = [
-    { text: 'Gen 1', region: 'Kanto', fillStyle: 'crimson', id: 1, weight: 1 },
-    { text: 'Gen 2', region: 'Johto', fillStyle: 'darkorange', id: 2, weight: 1 },
-    { text: 'Gen 3', region: 'Hoenn', fillStyle: 'green', id: 3, weight: 1 },
-    { text: 'Gen 4', region: 'Sinnoh', fillStyle: 'darkcyan', id: 4, weight: 1 },
-    { text: 'Gen 5', region: 'Unova', fillStyle: 'darkblue', id: 5, weight: 1 },
-    { text: 'Gen 6', region: 'Kalos', fillStyle: 'purple', id: 6, weight: 1 },
-    { text: 'Gen 7', region: 'Alola', fillStyle: 'orchid', id: 7, weight: 1 },
-    { text: 'Gen 8', region: 'Galar', fillStyle: 'black', id: 8, weight: 1 },
+    { text: 'Gen 1', region: 'Kanto', fillStyle: '#b71c1c', id: 1, weight: 1 },
+    { text: 'Gen 2', region: 'Johto', fillStyle: '#c65100', id: 2, weight: 1 },
+    { text: 'Gen 3', region: 'Hoenn', fillStyle: '#8d6e00', id: 3, weight: 1 },
+    { text: 'Gen 4', region: 'Sinnoh', fillStyle: '#2e7d32', id: 4, weight: 1 },
+    { text: 'Gen 5', region: 'Unova', fillStyle: '#006064', id: 5, weight: 1 },
+    { text: 'Gen 6', region: 'Kalos', fillStyle: '#0d47a1', id: 6, weight: 1 },
+    { text: 'Gen 7', region: 'Alola', fillStyle: '#283593', id: 7, weight: 1 },
+    { text: 'Gen 8', region: 'Galar', fillStyle: '#6a1b9a', id: 8, weight: 1 },
+    { text: 'Gen 9', region: 'Paldea', fillStyle: '#880e4f', id: 9, weight: 1 },
   ];
 
   private generation = new BehaviorSubject<GenerationItem>(this.generations[0]);
